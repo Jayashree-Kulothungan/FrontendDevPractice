662e9b3f556c31d8f1c9c302368390ee2830ef84
Jayashree-Kulothungan <jayashreekulothungan@gmail.com>
2025-09-09 00:21:48 -0300

    add semantic html

--- qr-code-component/src/App.jsx
+++ qr-code-component/src/App.jsx
@@ -3,24 +3,24 @@ import "./App.css";
 function App() {
 	return (
 		<>
-			<div>
-				<div className="card">
+			<main>
+				<article className="card">
 					<img
 						className="qrcode"
 						src="../images/image-qr-code.png"
 						alt="Avatar"
 					></img>
-					<div className="card-texts">
-						<p className="text-1">
+					<section className="card-texts">
+						<h1 className="text-1">
 							Improve your front-end skills by building projects
-						</p>
+						</h1>
 						<p className="text-2">
 							Scan the QR code to visit Frontend Mentor and take your coding
 							skills to the next level
 						</p>
-					</div>
-				</div>
-			</div>
+					</section>
+				</article>
+			</main>
 		</>
 	);
 }
